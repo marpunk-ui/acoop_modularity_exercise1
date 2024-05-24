f31b0a4a091d2333704a0a7687eda0e864709aa4
marpunk-ui <156810495+marpunk-ui@users.noreply.github.com>
2024-05-24 13:51:37 +0300

    Add files via upload

--- Modularity_e1_2.java
+++ Modularity_e1_2.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Modularity_e1_2 {
 
 public static float avg(int[] nums) throws EmptyArray, NegArray { 
-    int sum = 0;
+    float sum = 0;
     if (nums == null || nums.length == 0)
         throw new EmptyArray();
     ArrayList<Integer> nint = new ArrayList<>(0);;
